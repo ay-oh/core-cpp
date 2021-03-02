bbd6c0ac0506e2654f8051d3da07edd94fd3f902
cororing <corock.dev@gmail.com>
2021-03-02 18:48:08 +0900

    derived 클래스 생성자 및 소멸자 예제

--- 03--inheritance/04--ctors-n-init-of-derived-class.cpp
+++ 03--inheritance/04--ctors-n-init-of-derived-class.cpp
@@ -13,18 +13,13 @@ public:
     /**
      * TODO Task 1: Animal 클래스의 생성자를 추가하세요.
      */
-    Animal()
-    {
-        cout << "Animal constructor() " << endl;
-    }
+
+
 
     /**
      * TODO Task 2: Animal 클래스의 소멸자를 추가하세요.
      */
-    ~Animal()
-    {
-        cout << "Animal destructor() " << endl;
-    }
+
 };
 
 
@@ -36,18 +31,13 @@ public:
     /**
      * TODO Task 3: Dog 클래스의 생성자를 추가하세요.
      */
-    Dog()
-    {
-        cout << "Dog constructor() " << endl;
-    }
+
 
     /**
      * TODO Task 4: Dog 클래스의 소멸자를 추가하세요.
      */
-    ~Dog()
-    {
-        cout << "Dog constructor() " << endl;
-    }
+
+
 };
 
 
